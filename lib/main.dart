dd0375a31cae2a72eb12272214146c2967c1e260
amoslucky <aluckya100@gmail.com>
2025-10-25 11:42:49 -0400

    successful launch in mobile phone / Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // lib/main.dart
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
@@ -19,6 +20,15 @@ FirebaseOptions firebaseOption = FirebaseOptions(
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: firebaseOption); // requires google-services.json / plist set up
+  if(kIsWeb){
+     await Firebase.initializeApp(options: firebaseOption); // requires google-services.json / plist set up
+
+  }else{
+     await Firebase.initializeApp(
+      //options: DefaultFirebaseOptions.currentPlatform,
+     );
+
+  }
+ 
   runApp(const NaijaWhotApp());
 }
